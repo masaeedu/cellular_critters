9f5f178c2c9ecb8ba405ea7ae89c771b2d5d5ba9
Asad Saeeduddin <masaeedu@gmail.com>
2019-02-25 19:44:47 -0500

    Properly factor out effectful stuff

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const Canvas = require("terminal-canvas");
 const {
   Fn,
   Arr,
+  Obj,
   Fnctr,
+  Cont,
   cata,
   ana,
   hylo,
@@ -59,13 +61,31 @@ const LazyList = (() => {
 
   const LazyList = { Nil, Cons, match };
 
+  // :: x -> LazyList x
+  const repeat = ana(Base)(x => Cons(x)(x));
+
+  // :: (a -> b -> c) -> LazyList a -> LazyList b -> LazyList c
+  const zipWith = f => l1 => l2 =>
+    Fn.flip(match)(l1)({
+      Nil,
+      Cons: x => xs =>
+        Fn.flip(match)(l2)({
+          Nil,
+          Cons: y => ys => Cons(f(x)(y))(zipWith(f)(xs)(ys))
+        })
+    });
+
+  // :: Applicative LazyList
+  const Zip = {
+    of: repeat,
+    lift2: zipWith
+  };
+
   // :: Int -> LazyList Int
   const range = n => ana(Base)(i => (i === n ? Nil : Cons(i)(i + 1)))(0);
 
-  // :: [x] -> LazyList x
-  const fromArray = ana(Base)(Arr.match(LazyList));
-  // :: LazyList x -> [x]
-  const toArray = cata(Base)(Fn["<:"](List.match(Arr))(Lazy.force));
+  // :: LazyList Int
+  const nats = range(Infinity);
 
   // :: (a -> b) -> LazyList a -> LazyList b
   const map = f => cata(Base)(match({ Nil, Cons: x => Cons(f(x)) }));
@@ -86,17 +106,37 @@ const LazyList = (() => {
       Cons: x => xs => Cons(x)(append(xs)(ys))
     })(xs);
 
+  // :: Monad m -> (a -> b -> m a) -> m a -> LazyList b -> m a
+  const foldM = M => f => z =>
+    Fn.pipe([
+      Lazy.force,
+      List.match({
+        Nil: M.of(z),
+        Cons: x => xs => M[">>="](f(z)(x))(a => foldM(M)(f)(a)(xs))
+      })
+    ]);
+
+  // :: [x] -> LazyList x
+  const fromArray = ana(Base)(Arr.match(LazyList));
+  // :: LazyList x -> [x]
+  const toArray = cata(Base)(Fn["<:"](List.match(Arr))(Lazy.force));
+
   return {
     Base,
     Nil,
     Cons,
     match,
+    repeat,
+    zipWith,
+    Zip,
     range,
-    fromArray,
-    toArray,
+    nats,
     map,
     take,
-    append
+    append,
+    foldM,
+    fromArray,
+    toArray
   };
 })();
 const LL = LazyList;
@@ -115,11 +155,56 @@ const Vec = (() => {
   return { rotate, add };
 })();
 
+// :: Byte -> Hex
+const byte2hex = n => {
+  const nybHexString = "0123456789ABCDEF";
+  return (
+    String(nybHexString.substr((n >> 4) & 0x0f, 1)) +
+    nybHexString.substr(n & 0x0f, 1)
+  );
+};
+
+// :: [Byte, Byte, Byte] -> Color
+const rgb2hex = ([r, g, b]) => `#${byte2hex(r)}${byte2hex(g)}${byte2hex(b)}`;
+
+// :: type Signal v = Real -> v
+const Signal = (() => {
+  // :: Signal Real
+  const { sin } = Math;
+  // :: Frequency -> Signal Real -> Signal Real
+  const accelerate = freq => Fn.contramap(x => x * freq);
+  // :: Real -> Signal Real -> Signal Real
+  const amplify = a => Fn.map(y => y * a);
+  // :: Real -> Signal Real -> Signal Real
+  const vshift = v => Fn.map(y => y + v);
+  // :: Real -> Signal v -> Signal v
+  const phase = dx => Fn.contramap(x => x + dx);
+  // :: Signal Color
+  const rainbow = f =>
+    Fn.passthru([0, 2, 4])([
+      Arr.traverse(Fn)(p =>
+        Fn.passthru(sin)([phase(p), accelerate(f), amplify(127), vshift(128)])
+      ),
+      Fn.map(rgb2hex)
+    ]);
+
+  return { rainbow };
+})();
+
+const Cont_ = (() => {
+  const delay = v => d => cb => setTimeout(() => cb(v), d);
+  const delay_ = delay(undefined);
+  const runCont = c => c(_ => {});
+
+  return { delay, delay_, runCont };
+})();
+
 // ## MAIN ##
 
 // :: type Step = { leg: LazyList Vec2, transform: Transform2 }
 
-const x_axis = LL.map(x => [x, 0])(LL.range(Infinity));
+// :: LazyList Vec2
+const x_axis = LL.map(x => [x, 0])(LL.nats);
 
 // :: Vec2 -> LazyListF Step Vec2
 const inwards = ([w, h]) => {
@@ -133,12 +218,7 @@ const inwards = ([w, h]) => {
 const outwards = i => {
   const n = Math.floor(i / 2) + 1;
   const leg = LL.take(n)(x_axis);
-  const transform = Fn.pipe([
-    // 90 deg ccw rotation
-    Vec.rotate(Math.PI / 2),
-    // Rightwards displacement by the length of the leg we rendered
-    Vec.add([n, 0])
-  ]);
+  const transform = Fn.pipe([Vec.rotate(-Math.PI / 2), Vec.add([n, 0])]);
 
   return Cons({ leg, transform })(i + 1);
 };
@@ -149,26 +229,26 @@ const trace = LL.match({
   Cons: ({ leg, transform }) => Fn.pipe([LL.map(transform), LL.append(leg)])
 });
 
-const result = hylo(LL.Base)(trace)(inwards)([20, 20]);
+const spiral = hylo(LL.Base)(trace)(outwards)(0);
 
-// ## TEST ##
+// ## DRAW ##
+const rainbowSpiral = Obj.sequence(LL.Zip)({
+  i: LL.nats,
+  color: LL.map(Signal.rainbow(0.05))(LL.nats),
+  point: spiral
+});
 const origin = [process.stdout.columns / 2, process.stdout.rows / 2];
 const canvas = new Canvas();
-const renderPoint = x =>
-  Lazy.defer(_ =>
-    canvas
-      .moveTo(...x)
-      .foreground("red")
-      .write("*")
-      .flush()
-  );
+const renderPoint = ({ i, color, point }) => cb => {
+  canvas
+    .moveTo(...Vec.add(origin)(point))
+    .foreground(color)
+    .write((i % 10).toString())
+    .flush();
+  cb(undefined);
+};
+const renderAndWait = p =>
+  Arr.sequence(Cont)([renderPoint(p), Cont_.delay_(50)]);
+const main = LL.foldM(Cont)(Fn.const(renderAndWait))(undefined)(rainbowSpiral);
 
-const interpret = LL.match({
-  Nil: Lazy.of(undefined),
-  Cons: point => rest =>
-    Lazy["*>"](renderPoint(Vec.add(origin)(point)))(_ =>
-      setTimeout(interpret(rest), 5)
-    )
-});
-
-interpret(result)();
+Cont_.runCont(main);
